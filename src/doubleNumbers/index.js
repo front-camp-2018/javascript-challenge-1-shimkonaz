3ec8703492842740c57d9565acaa9ed3efb64f7f
Unknown <nazarkoo4435@gmail.com>
2018-11-01 23:25:31 +0200

    doubleNumbers & sortByWeight tasks done

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,12 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const numbered = num.toString();
+  const head = numbered.substring(0, numbered.length / 2);
+  const tail = numbered.substring(numbered.length / 2);
+
+  if (head === tail) {
+    return num;
+  } else {
+    return num * 2;
+  }
 };
--- src/sortByWeight/index.js
+++ src/sortByWeight/index.js
@@ -1,4 +1,10 @@
 /* eslint-disable-next-line */
 export const sortByWeight = arr => {
-  // your logic here...
+  return arr.sort((first, second) => sumNums(first) > sumNums(second));
+};
+
+const sumNums = number => {
+  return number.split('').reduce(
+    (sum, curr) => parseInt(sum) + parseInt(curr)
+  );
 };
